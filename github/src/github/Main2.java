c81d44767a1aa580e52d76d830df7823f0aa95e7
Nikita <Nikita@ASUS>
2024-07-26 11:18:14 +0530

    new me changes

--- github/src/github/Main2.java
+++ github/src/github/Main2.java
@@ -2,6 +2,7 @@ package github;
 
 public class Main2 {
  public static void main(String[] args) {
-	System.out.println("hogya 2nd branch?");
+	System.out.println("hogya");
+	System.out.println("pulled or not?");
 }
 }
